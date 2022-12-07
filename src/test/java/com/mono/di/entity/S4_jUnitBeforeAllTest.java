a3366f4ce9419f82e2830ac7306aaf1d6a51f5a7
최승은 <liberalfilm@gmail.com>
2022-12-07 21:17:24 +0900

    2022-12-07-Wed-21:17

---
+++ src/test/java/com/mono/di/entity/S4_jUnitBeforeAllTest.java
@@ -0,0 +1,37 @@
+package com.mono.di.entity;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class S4_jUnitBeforeAllTest {
+
+    private static ApplicationContext ctx;
+
+    @BeforeAll
+    public static void beforeAllMessage() {
+        ctx = new ClassPathXmlApplicationContext("classpath:config1.xml");
+        System.out.println("BeforeAll.ctx : " + ctx);
+    }
+
+    @Test
+    @DisplayName("springContainerTest1의 BeforeAll this 확인")
+    public void springContainerTest1() {
+        System.out.println("springContainerTest1 : " + this);
+        System.out.println("springContainerTest1.ctx : " + ctx);
+        Hello hello = ctx.getBean(Hello.class);
+        hello.print("hello...");
+    }
+
+    @Test
+    @DisplayName("springContainerTest1의 BeforeAll this 확인")
+    public void springContainerTest2() {
+        System.out.println("springContainerTest2 : " + this);
+        System.out.println("springContainerTest2.ctx : " + ctx);
+        Hello hello = ctx.getBean(Hello.class);
+        hello.print("hello...");
+    }
+}
+
